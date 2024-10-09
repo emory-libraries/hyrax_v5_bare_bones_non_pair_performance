d52ab27c9e50c5da939708e082df068a021ae6d1
bwatson78 <brad.watson.orlando@gmail.com>
2024-10-09 07:30:10 -0500

    removes secondary visibility assignment.

--- lib/mass_work_generation_performance_test.rb
+++ lib/mass_work_generation_performance_test.rb
@@ -21,7 +21,6 @@ class MassWorkGenerationPerformanceTest
       'change_set.set_user_as_depositor' => { user: @user },
       'collection_resource.apply_collection_type_permissions' => { user: @user }
     ).call(@coll_change_set).value_or {}
-    apply_open_visibility(resource: @persisted_collection)
   end
 
   def create_works_in_collection
@@ -29,14 +28,13 @@ class MassWorkGenerationPerformanceTest
     for i in 1..@number_of_works do
       create_uploaded_file(i)
       create_transaction_requirements_for_work(i)
-      persisted_work = @work_tx.with_step_args(
+      @work_tx.with_step_args(
         'work_resource.add_file_sets' => { uploaded_files: [@uploaded_file] },
         'change_set.set_user_as_depositor' => { user: @user },
         'work_resource.change_depositor' => { user: @user },
         'work_resource.save_acl' => { permissions_params: {} }
-      ).call(@work_change_set).value_or {}
+      ).call(@work_change_set)
       @file.unlink
-      apply_open_visibility(resource: persisted_work)
       if i.multiple_of?(100)
         report_current_time
         sleep 0.5
@@ -48,7 +46,8 @@ class MassWorkGenerationPerformanceTest
     collection_type = Hyrax::CollectionType.find_or_create_default_collection_type
     testing_collection = Collection.new(title: ["Non-Pair-Tree Testing Collection"],
                                         creator: ["A Testing User"],
-                                        collection_type_gid: collection_type.to_global_id.to_s)
+                                        collection_type_gid: collection_type.to_global_id.to_s,
+                                        visibility: 'open')
     @coll_change_set = Hyrax::ChangeSet.for(testing_collection)
     @collection_transaction = Hyrax::Transactions::Container['change_set.create_collection']
   end
@@ -56,7 +55,8 @@ class MassWorkGenerationPerformanceTest
   def create_transaction_requirements_for_work(ind)
     testing_work = GenericWork.new(title: ["Non-Pair-Tree Testing Work #{ind}"],
                                    creator: ["A Testing User"],
-                                   member_of_collection_ids: [@persisted_collection.id])
+                                   member_of_collection_ids: [@persisted_collection.id],
+                                   visibility: 'open')
     @work_change_set = Hyrax::ChangeSet.for(testing_work)
     @work_tx = Hyrax::Transactions::Container['change_set.create_work']
   end
@@ -65,16 +65,6 @@ class MassWorkGenerationPerformanceTest
     @file = Tempfile.new("text_file_#{ind}")
     @file.write("Text for #{ind}")
     @uploaded_file = Hyrax::UploadedFile.create(file: @file, user: @user)
-  end
-
-  def apply_open_visibility(resource:)
-    resource.visibility = 'open'
-    persist_and_reindex(resource:)
-  end
-
-  def persist_and_reindex(resource:)
-    Hyrax.persister.save(resource:)
-    Hyrax.index_adapter.save(resource:)
   end
 
   def report_current_time
